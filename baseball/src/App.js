f582e70de0a1b2be619ff618a59a238073e610ba
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-03 14:13:05 -0700

    working on the display components

---
+++ baseball/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>the baseball scores will be displayed right below this line of text</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
